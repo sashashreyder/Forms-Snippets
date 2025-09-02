1e6073e2df0208b32e3d78bac6a45dfd732e5ce3
Sasha <167915085+sashashreyder@users.noreply.github.com>
2025-09-02 13:57:54 -0300

    Update Donation-Form.tsx

--- Donation-Form/Donation-Form.tsx
+++ Donation-Form/Donation-Form.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
-import styles from "./App.module.css";
+import styles from "./DonationForm.module.css";
 
 type FormData = {
   name: string;
