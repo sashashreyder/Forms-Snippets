d3397c4062e7d26a98af372839c07bc2c7a2aff9
Sasha <167915085+sashashreyder@users.noreply.github.com>
2025-09-08 12:37:41 -0300

    Create index.tsx

---
+++ Toast-Notifications/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { ToastProvider } from "./Toast";
+
+const rootElement = document.getElementById("root")!;
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(
+  <React.StrictMode>
+    <ToastProvider>
+      <App />
+    </ToastProvider>
+  </React.StrictMode>
+);
